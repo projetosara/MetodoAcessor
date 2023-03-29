0b9502a3bd0b067af03c4af6907ffef0e36f5ea2
projetosara <100379644+projetosara@users.noreply.github.com>
2023-03-29 16:58:46 -0300

    Add files via upload

---
+++ Exemplo_Construtor/Produto.cs
@@ -0,0 +1,24 @@
+﻿namespace Exemplo_Construtor;
+class Produto
+{
+    public Produto(){
+        this.Nome = "XPTO";
+        this.Preco = 1;
+    }
+    
+    public Produto(double preco){
+        this.Nome = "NÃO INFORMADO";
+        this.Preco = preco;
+    }
+    
+    public Produto(string nome,double desconto){
+        this.Nome = nome;
+        this.Preco = 10 - 10*desconto/100;
+    }
+   public string? Nome;
+   public double Preco;
+
+   public void MostraDados(){}
+
+   
+}
---
+++ Exemplo_Construtor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exemplo_Construtor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1= new Produto(100);
+        
+        p1.MostraDados();
+    }
+}
