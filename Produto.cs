33f641f97b7c79cd7d05a83964a11bfb6c19d90d
projetosara <100379644+projetosara@users.noreply.github.com>
2023-03-29 16:26:26 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace Exemplo_Acessor;
+class Produto
+{
+   public string? Nome;
+   public double Preco;
+
+   public void MostraDados(){
+    Console.WriteLine("Nome: "+ this.Nome);
+    Console.WriteLine("Preço: "+ this.Preco);
+   }
+   public void AlteraPreco(double preco){
+        if(preco >=0){
+            this.Preco = preco;
+        }else{
+            Console.WriteLine("VALOR INVÁLIDO!");
+        }
+   }
+   public void AlteraNome(string nome){
+    this.Nome = nome;
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exemplo_Acessor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto();
+        p1.AlteraPreco(10);
+        p1.AlteraNome("PARAFUSO");
+        p1.MostraDados();
+    }
+}
